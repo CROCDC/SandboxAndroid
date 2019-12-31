57ee9c20e94444177bd7e48ed048a641bad78183
camilo <cromero@yopapp.me>
2019-12-31 17:45:05 -0300

    added provider

--- app/src/main/java/com/cr/o/cdc/sandboxAndroid/repos/PokemonDataSource.kt
+++ app/src/main/java/com/cr/o/cdc/sandboxAndroid/repos/PokemonDataSource.kt
@@ -1,16 +1,18 @@
 package com.cr.o.cdc.sandboxAndroid.repos
 
 import PokemonQuery
+import androidx.lifecycle.LiveData
 import com.apollographql.apollo.ApolloClient
 import com.cr.o.cdc.sandboxAndroid.network.ApolloManager
+import com.cr.o.cdc.sandboxAndroid.network.NetworkResponse
 import okhttp3.OkHttpClient
 
-class PokemonDataSource(private val apolloManager: ApolloManager) {
+class PokemonDataSource(private val apolloManager: ApolloManager) : PokemonDataSourceProvider {
 
     private val client = ApolloClient.builder().serverUrl("https://graphql-pokemon.now.sh/graphql")
         .okHttpClient(OkHttpClient.Builder().build())
         .build()
 
-    fun pokemon(id: String) =
+    override fun pokemon(id: String): LiveData<NetworkResponse<PokemonQuery.Data>> =
         apolloManager.request(client.query(PokemonQuery.builder().id(id).build()))
 }
--- app/src/main/java/com/cr/o/cdc/sandboxAndroid/repos/PokemonDataSourceProvider.kt
+++ app/src/main/java/com/cr/o/cdc/sandboxAndroid/repos/PokemonDataSourceProvider.kt
@@ -1,7 +1,12 @@
 package com.cr.o.cdc.sandboxAndroid.repos
 
+import PokemonQuery
+import androidx.lifecycle.LiveData
+import com.cr.o.cdc.sandboxAndroid.network.NetworkResponse
+
 /**
  * Created by Camilo on 31/12/19.
  */
 interface PokemonDataSourceProvider {
+    fun pokemon(id: String): LiveData<NetworkResponse<PokemonQuery.Data>>
 }
