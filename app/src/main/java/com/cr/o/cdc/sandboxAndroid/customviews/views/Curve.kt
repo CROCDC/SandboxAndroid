d93d27d40493221a9cd2f8c4702e99c143b9de84
camilo <cromero@yopdev.com>
2020-02-25 21:47:30 -0300

    change curve

--- app/src/main/java/com/cr/o/cdc/sandboxAndroid/customviews/views/Curve.kt
+++ app/src/main/java/com/cr/o/cdc/sandboxAndroid/customviews/views/Curve.kt
@@ -7,9 +7,6 @@ import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
-import kotlin.math.atan2
-import kotlin.math.cos
-import kotlin.math.sin
 
 class Curve @JvmOverloads constructor(
     context: Context,
@@ -17,58 +14,29 @@ class Curve @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-    private var size = 0
-
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        drawCurvedArrow(
-            canvas,
-            size * 0.2,// x start
-            size * 0.6,// y start
-            size * 0.8,//x end
-            size * 0.6,// y end
-            size * 0.2,// curve
-            Color.MAGENTA,
-            20
-        )
+        drawCurvedArrow(canvas)
     }
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        size = measuredWidth.coerceAtMost(measuredHeight)
-        setMeasuredDimension(size, size)
-
-    }
-
-    private fun drawCurvedArrow(
-        canvas: Canvas,
-        x1: Double,
-        y1: Double,
-        x2: Double,
-        y2: Double,
-        curveRadius: Double,
-        color: Int,
-        lineWidth: Int
-    ) {
+    private fun drawCurvedArrow(canvas: Canvas) {
 
         val paint = Paint()
         paint.isAntiAlias = true
         paint.style = Paint.Style.STROKE
-        paint.strokeWidth = lineWidth.toFloat()
-        paint.color = color
+        paint.strokeWidth = 20F
+        paint.color = Color.MAGENTA
 
         val path = Path()
-        val midX = x1 + (x2 - x1) / 2
-        val midY = y1 + (y2 - y1) / 2
-        val xDiff = (midX - x1).toFloat()
-        val yDiff = (midY - y1).toFloat()
-        val angle = atan2(yDiff.toDouble(), xDiff.toDouble()) * (180 / Math.PI) - 90
-        val angleRadians = Math.toRadians(angle)
-        val pointX = (midX + curveRadius * cos(angleRadians)).toFloat()
-        val pointY = (midY + curveRadius * sin(angleRadians)).toFloat()
-
-        path.moveTo(x1.toFloat(), y1.toFloat())
-        path.cubicTo(x1.toFloat(), y1.toFloat(), pointX, pointY, x2.toFloat(), y2.toFloat())
+        path.moveTo(0F, height.toFloat())
+        path.cubicTo(
+            0F,
+            height.toFloat(),
+            (width / 2).toFloat(),
+            -height.toFloat(),
+            width.toFloat(),
+            height.toFloat()
+        )
         canvas.drawPath(path, paint)
     }
 }
