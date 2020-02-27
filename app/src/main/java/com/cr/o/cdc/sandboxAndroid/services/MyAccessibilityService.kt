b76ca07b88ddd2dbc6031d85bfced7a33184c5e1
camilo <cromero@yopdev.com>
2020-02-27 09:26:36 -0300

    finish whatsapp list

---
+++ app/src/main/java/com/cr/o/cdc/sandboxAndroid/services/MyAccessibilityService.kt
@@ -0,0 +1,16 @@
+package com.cr.o.cdc.sandboxAndroid.services
+
+import android.accessibilityservice.AccessibilityService
+import android.util.Log
+import android.view.accessibility.AccessibilityEvent
+
+class MyAccessibilityService : AccessibilityService() {
+    override fun onInterrupt() {}
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent) {
+        if (event.packageName == "com.whatsapp") {
+            Log.e("CROCDC", event.toString())
+        }
+    }
+
+}
--- app/src/main/java/com/cr/o/cdc/sandboxAndroid/whatsapputils/model/WhatsappMessage.kt
+++ app/src/main/java/com/cr/o/cdc/sandboxAndroid/whatsapputils/model/WhatsappMessage.kt
@@ -4,4 +4,4 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class WhatsappMessage(@PrimaryKey(autoGenerate = true) val id: Int, val message: String?, val contact: String?)
+data class WhatsappMessage(@PrimaryKey val message: String, val contact: String?)
