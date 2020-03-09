f0d178927b00f9bdc1214a45f33b864d8b4364ea
Camilo Romero <romerocamilo03@gmail.com>
2020-03-09 09:50:30 -0300

    added tests

---
+++ app/src/androidTest/java/com/cr/o/cdc/customviews/fragments/CustomViewFragmentTest.kt
@@ -0,0 +1,31 @@
+package com.cr.o.cdc.customviews.fragments
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import com.cr.o.cdc.sandboxAndroid.R
+import com.cr.o.cdc.sandboxAndroid.customviews.fragments.CustomViewsFragment
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Test
+
+class CustomViewFragmentTest {
+
+    @Test
+    fun assertNavigateToCurveFragment() {
+        val navController = mockk<NavController>(relaxed = true)
+        launchFragmentInContainer<CustomViewsFragment>().onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+        }
+
+        Espresso.onView(ViewMatchers.withId(R.id.btn_curve))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed())).perform(click())
+
+        verify { navController.navigate(R.id.action_customViewsFragment_to_curveFragment) }
+    }
+
+}
--- app/src/androidTest/java/com/cr/o/cdc/sandboxAndroid/db/WhatsappMessageDaoTest.kt
+++ app/src/androidTest/java/com/cr/o/cdc/sandboxAndroid/db/WhatsappMessageDaoTest.kt
@@ -2,7 +2,7 @@ package com.cr.o.cdc.sandboxAndroid.db
 
 import com.cr.o.cdc.sandboxAndroid.utils.DBTest
 import com.cr.o.cdc.sandboxAndroid.whatsapputils.model.WhatsappMessage
-import com.cr.o.cdc.sharedtest.getValueLivedata
+import com.cr.o.cdc.sharedtest.getValueLiveData
 import com.cr.o.cdc.sharedtest.makeRandomInstance
 import junit.framework.TestCase.assertTrue
 import org.junit.Test
@@ -11,7 +11,7 @@ class WhatsappMessageDaoTest : DBTest() {
 
     private val dao = db.whatsappMessageDao()
 
-    val whatsappMessage: WhatsappMessage = WhatsappMessage::class.makeRandomInstance()
+    private val whatsappMessage: WhatsappMessage = WhatsappMessage::class.makeRandomInstance()
 
     @Test
     fun save() {
@@ -22,7 +22,7 @@ class WhatsappMessageDaoTest : DBTest() {
     fun loadAll() {
         dao.save(whatsappMessage)
 
-        assertTrue(getValueLivedata(dao.loadAll(), 2)?.isNotEmpty() == true)
+        assertTrue(getValueLiveData(dao.loadAll(), 2)?.isNotEmpty() == true)
     }
 
 }
