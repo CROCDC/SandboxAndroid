d178d41521fb82508aff8242f56d61ddd6bf1cd2
Camilo Romero <romerocamilo03@gmail.com>
2020-06-03 19:07:52 -0300

    lint

--- app/src/main/java/com/cr/o/cdc/sandboxAndroid/motionlayout/fragments/MotionLayoutSelectorFragment.kt
+++ app/src/main/java/com/cr/o/cdc/sandboxAndroid/motionlayout/fragments/MotionLayoutSelectorFragment.kt
@@ -28,5 +28,4 @@ class MotionLayoutSelectorFragment : Fragment() {
 
         return binding.root
     }
-
 }
--- app/src/main/java/com/cr/o/cdc/sandboxAndroid/motionlayout/fragments/SpaceRocketFragment.kt
+++ app/src/main/java/com/cr/o/cdc/sandboxAndroid/motionlayout/fragments/SpaceRocketFragment.kt
@@ -10,10 +10,8 @@ import com.cr.o.cdc.sandboxAndroid.R
 class SpaceRocketFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_space_rocket, container, false)
-    }
-
+    ): View? = inflater.inflate(R.layout.fragment_space_rocket, container, false)
 }
