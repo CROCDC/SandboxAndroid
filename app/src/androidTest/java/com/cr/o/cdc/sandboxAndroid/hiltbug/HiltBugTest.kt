7ef83f86bbd56b59a24f9de82c8697ad432a6456
cromero@yopapp.me <Monroe4500>
2021-05-09 12:30:59 -0300

    added HiltAndroidRule

--- app/src/androidTest/java/com/cr/o/cdc/sandboxAndroid/hiltbug/HiltBugTest.kt
+++ app/src/androidTest/java/com/cr/o/cdc/sandboxAndroid/hiltbug/HiltBugTest.kt
@@ -2,8 +2,13 @@ package com.cr.o.cdc.sandboxAndroid.hiltbug
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.test.core.app.ActivityScenario
+import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+
 
 /**
  * Created by Cami on 5/8/21.
@@ -11,7 +16,15 @@ import org.junit.Test
 @HiltAndroidTest
 class HiltBugTest {
 
+    @Rule
+    var hiltRule = HiltAndroidRule(this)
+
     lateinit var scenario: ActivityScenario<out AppCompatActivity>
+
+    @Before
+    fun before(){
+        hiltRule.inject()
+    }
 
     @Test
     fun test(){
